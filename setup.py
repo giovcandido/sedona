da3d9fa3da18dbe73d9be7e440102b480320e6d9
giovcandido <giovcandido@outlook.com>
2021-08-24 17:38:21 -0300

    Create setup.py with some project informations

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'sedona',
+    version = '0.1.0',
+    author = 'Giovani Candido',
+    author_email = 'giovcandido@outlook.com',
+    license = 'GNU General Public License v3.0',
+    description = 'Sedona is a free youtube mp3 downloader made with Python.',
+    long_description = '',
+    long_description_content_type = '',
+    url = 'https://github.com/giovcandido/sedona',
+    py_modules = [],
+    packages = find_packages(),
+    install_requires = [],
+    python_requires = '>=3.8',
+    classifiers = [
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: Cross-platform"
+    ],
+    entry_points = []
+)
