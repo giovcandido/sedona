f5a481ab2c52728fb7f5419560a0d9efa4a91333
giovcandido <giovcandido@outlook.com>
2021-08-24 18:00:32 -0300

    Include entry_point in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup, find_packages
 
+# Load long description 
+with open("README.md", encoding='utf-8') as f:
+    long_description = f.read()
+
+# Load requirements
+with open("requirements.txt", encoding='utf-8') as f:
+    requirements = f.read()
+
+# Configure setup 
 setup(
     name = 'sedona',
     version = '0.1.0',
@@ -7,16 +16,19 @@ setup(
     author_email = 'giovcandido@outlook.com',
     license = 'GNU General Public License v3.0',
     description = 'Sedona is a free youtube mp3 downloader made with Python.',
-    long_description = '',
-    long_description_content_type = '',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
     url = 'https://github.com/giovcandido/sedona',
-    py_modules = [],
+    py_modules = ['sedona', 'modules'],
     packages = find_packages(),
-    install_requires = [],
+    install_requires = [requirements],
     python_requires = '>=3.8',
     classifiers = [
         "Programming Language :: Python :: 3.8",
         "Operating System :: Cross-platform"
     ],
-    entry_points = []
+    entry_points = '''
+        [console_scripts]
+        sedona=sedona:main
+    '''
 )
