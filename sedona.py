3dbf536604f6c0f7a6e4d91eb8ba91a3c4665f65
giovcandido <giovcandido@outlook.com>
2021-08-26 11:09:41 -0300

    Test Downloader.py functionalities

--- sedona.py
+++ sedona.py
@@ -1,5 +1,20 @@
-def main():
-    print('Sedona says Hi!')
+from argparse import ArgumentParser
 
-if __name__ == '__main__':
-    main()
+from modules.Downloader import Downloader
+
+def parse_arguments():
+    parser = ArgumentParser(description="Sedona is a free youtube downloader and mp3 converter.")
+    
+    parser.add_argument('url', metavar='URL', type=str, help="Video URL")
+
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()
+
+    video_url = args.url
+
+    downloader = Downloader(video_url)
+
+    print(downloader.url)
+    print(downloader.title)
