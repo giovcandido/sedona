6b01ebf324d7388ce7b38b78c17aa63ba3a1ca44
Davi Neves <davi.neves@unesp.br>
2021-09-01 17:57:58 -0300

    Update __main__.py

--- sedona/__main__.py
+++ sedona/__main__.py
@@ -9,13 +9,16 @@ from .Converter import Converter
 from pytube.helpers import safe_filename
 
 def main():
+    # Getting proper arguments from the terminal
     args = parse_arguments()
 
+    # Creating a list with multiples urls (if exists)
     urls = list()
 
     # Checking if the URL is text file or not
     if args.url.endswith('.txt'):
         try:
+            # If its a text file, save all URL's in the variable for later download and conversion
             text_file = args.url
 
             with open(text_file) as f:
@@ -25,6 +28,7 @@ def main():
 
             sys_exit(1)
     else:
+        # If isn't a text file, save all URL's present in the arguments from terminal
         urls.append(args.url)
     
     # Downloading multiple url's
@@ -38,13 +42,15 @@ def main():
             
                 print('Downloading "%s"...' % (video.title))
 
+                # Download the song in video format and return the correct path for conversion
                 video_path = video.download_audio_stream()
                 
-                # Get video and convert it to mp3 to sedona directory
+                # Get video path and convert it to mp3 to "sedonaMP3" directory in home
                 converter = Converter(video_path)
 
                 print('Converting downloaded video to mp3...')
 
+                # Convert the song to MP3, with a default bitrate of 256kbps
                 converter.convert_audio_stream(video.filename)
 
                 print('Done! File saved to your home directory.\n')
@@ -70,20 +76,22 @@ def main():
                     print("Position: " + str((position + 1)))
                     print("Title: " + video.filename)
                     
-                    # Download the track
+                    # Download the song in video format, with the track number in playlist, and return the correct path for conversion
                     video_path = video.download_audio_stream(track_number)
 
-                    # Get video and convert it to mp3 to playlist directory, in sedona directory
+                    # Update the playlist name to one allowed by the OS ("safe_filename"), upon creation of the specific directory
                     playlist_dir = safe_filename(playlist.title)
 
                     # Remove whitespaces from init and end of string
                     playlist_dir = playlist_dir.lstrip()
                     playlist_dir = playlist_dir.rstrip()
 
+                    # Get video path and convert it to mp3 to playlist directory, in "sedonaMP3" directory
                     converter = Converter(video_path)
 
                     print('Converting downloaded video to mp3...')
 
+                    # Convert the song to MP3, with his track number and a default bitrate of 256kbps
                     converter.convert_audio_stream(track_number + video.filename, playlist_dir)
 
                     print('Done! File saved to your home directory.\n')
