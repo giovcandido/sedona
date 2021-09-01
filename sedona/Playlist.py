b1be5c67ea04904550ebe831db1bd9b435280c54
giovcandido <giovcandido@outlook.com>
2021-09-01 12:42:52 -0300

    Make Playlist class iterable

--- sedona/Playlist.py
+++ sedona/Playlist.py
@@ -1,7 +1,5 @@
 from pytube import Playlist as PytubePlaylist
 
-from .Video import Video
-
 class Playlist:
 
     def __init__(self, url = None):
@@ -28,30 +26,8 @@ class Playlist:
     def __create_playlist(self):
         self.__playlist = PytubePlaylist(self.__url)
 
-    def download_all(self):
-        
-        # List of title and path to output
-        title_playlist_files = list()
-        output_files = list()
+    def __iter__(self):
+        return self.__playlist.video_urls.__iter__()
 
-        # Every object in "video_urls" of a Playlist is a URL of Youtube
-        for position, video_url in enumerate(self.__playlist.video_urls):
-            # Download youtube video to temp directory
-            video = Video(video_url)
-
-            track_number = str((position + 1)) + "." + " "
-
-            # Showing the track position and his title
-            print("\nPosition: " + str((position + 1)))
-            print("Title: " + video.filename)
-            
-            # Download the track
-            video_path = video.download_audio_stream(track_number)
-
-            # Save his track title (for convertion)
-            title_playlist_files.append(track_number + video.filename)
-
-            # Save his temp path to a list (for convertion)
-            output_files.append(video_path)
-            
-        return title_playlist_files, output_files
+    def __next__(self):
+        return self.__playlist.video_urls.__next__
